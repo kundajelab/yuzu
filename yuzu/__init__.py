4aaef7e73b19e770447e56fbe5c4d2b234d2838e
Jacob Schreiber <jmschreiber91@gmail.com>
2021-09-05 23:11:08 -0700

    FIX precompute

--- yuzu/__init__.py
+++ yuzu/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 # Author: Jacob Schreiber <jmschreiber91@gmail.com>
 
-from .yuzu_ism import precompute
+from .precompute import precompute
 from .yuzu_ism import yuzu_ism
 
 from .naive_ism import naive_ism
